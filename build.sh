d1f72ea0a6a30d7abf42c3fb494a7e9cd49b2c44
Joshua Gwinn <jdgbolt@gmail.com>
2022-07-31 20:23:56 -0700

    Add build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SCRIPT_PATH=$(dirname $(realpath $0))
+"$SCRIPT_PATH/node_modules/pnpm/bin/pnpm.cjs" run build
+rm -r "$SCRIPT_PATH/powertools/"
+mkdir "$SCRIPT_PATH/powertools"
+cp -r "$SCRIPT_PATH/dist" "$SCRIPT_PATH/LICENSE" "$SCRIPT_PATH/main.py" "$SCRIPT_PATH/package.json" "$SCRIPT_PATH/plugin.json" "$SCRIPT_PATH/README.md" "$SCRIPT_PATH/server.py" "$SCRIPT_PATH/powertools"
